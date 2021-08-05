77a274c02ae4d7ad0dc8cb24ab51ee5a582b7ec0
mjakobs2 <markus.jakobs@smail.th-koeln.de>
2021-08-06 00:11:59 +0100

    refactored gui

--- src/habedischnak/Main.java
+++ src/habedischnak/Main.java
@@ -1,6 +1,7 @@
 package habedischnak;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDate;
@@ -11,7 +12,10 @@ import java.time.LocalDate;
  */
 
 public class Main {
-    public static void calculate_moon_dates(){
+
+public static JFrame mainframe;
+
+public static void calculate_moon_dates(){
         double synodic_month = 29.530588;
 
         LocalDate historic_full_moon = LocalDate.of(2000,1,21);
@@ -37,23 +41,68 @@ public class Main {
         System.out.println("Next new moon is on "+nextNew);
 }
 
+public static void createGUI(){
+    mainframe = new JFrame("Moon Calculations");
+
+
+    JPanel panel = new JPanel();
+    panel.setLayout(new GridLayout(3,1));
+
+    JLabel labelheute = new JLabel("Heutiges Datum");
+    labelheute.setLocation(50,20);
+    labelheute.setVisible(true);
+    panel.add(labelheute);
+
+    JTextField datumseingabe = new JTextField("yyyy-mm-dd");
+    datumseingabe.setEditable(true);
+    datumseingabe.setLocation(100,20);
+    datumseingabe.setVisible(true);
+    panel.add(datumseingabe);
+
+
+    JLabel labelnextfull = new JLabel("Nächster Vollmond");
+    labelnextfull.setLocation(200,150);
+    labelnextfull.setVisible(true);
+    panel.add(labelnextfull);
+
+
+    JTextField vollmondausgabe = new JTextField("");
+    vollmondausgabe.setEditable(false);
+    vollmondausgabe.setLocation(200,50);
+    vollmondausgabe.setVisible(true);
+    panel.add(vollmondausgabe);
+
+
+    JButton calc = new JButton("Berechne");
+    calc.setEnabled(true);
+    calc.setLocation(150,300);
+    calc.setVisible(true);
+    panel.add(calc);
+
+
+
+    mainframe.getContentPane().add(panel);
+    mainframe.pack();
+    mainframe.show();
+
+}
+
+public LocalDate readToday(){
+
+    mainframe.getContentPane().getComponents().getClass();
+
+    return null;
+
+
+}
+
     public static void main(String[] args) throws IOException {
 	// write your code here
     //calculate_moon_dates();
 
-    /*Bare window*/
-    JFrame dateFrame = new JFrame("Next Moon");
-    //dateFrame.setLayout(null);
+    createGUI();
 
 
 
-
-    /*Labels*/
-    JLabel today = new JLabel("Heutiges Datum");
-    today.setLocation(50,50);
-
-    dateFrame.getContentPane().add(today);
-    dateFrame.setSize(640,480);
-    dateFrame.setVisible(true);
     }
 }
