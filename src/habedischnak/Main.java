816d0abcd2bc5cf7160215ed61233704a589a398
mjakobs2 <markus.jakobs@smail.th-koeln.de>
2021-08-05 23:36:03 +0100

    refactoring into function

--- src/habedischnak/Main.java
+++ src/habedischnak/Main.java
@@ -1,5 +1,6 @@
 package habedischnak;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDate;
 
@@ -8,33 +9,36 @@ import java.time.LocalDate;
  * Progamm zur Berechnung des jeweils kommenden Voll- / Neumondes
  */
 
-
 public class Main {
-    public static void main(String[] args) {
+    public static void calculate_moon_dates(){
+        double synodic_month = 29.530588;
+
+        LocalDate historic_full_moon = LocalDate.of(2000,1,21);
+        LocalDate today = LocalDate.now();
+        //LocalDate today = LocalDate.of(2021,9,25);
+        LocalDate nextFull, nextNew;
+
+        /*Get time between today and historic full moon & calculate cycles */
+        double elapsed = Duration.between(historic_full_moon.atStartOfDay(), today.atStartOfDay()).toDays();
+        double elapsed_cycles = elapsed/synodic_month;
+        double elapsed_nachkomma = (elapsed_cycles-((int) elapsed_cycles));
+
+        /*multiply percentage remainder times synodic month to get remaining days to the next full moon*/
+        double until_next_full = (1 - elapsed_nachkomma)*synodic_month;
+        nextFull = today.plusDays((long)until_next_full);
+
+        System.out.println("Next full moon is on "+nextFull);
+
+        /*multiply percentage remainder times synodic month to get remaining days to the next full moon*/
+        double until_next_new = (0.5 - elapsed_nachkomma)*synodic_month +1;
+        nextNew = today.plusDays((long)until_next_new);
+
+        System.out.println("Next new moon is on "+nextNew);
+}
+
+    public static void main(String[] args) throws IOException {
 	// write your code here
-    double synodic_month = 29.530588;
-
-	LocalDate historic_full_moon = LocalDate.of(2000,1,21);
-    LocalDate today = LocalDate.now();
-    //LocalDate today = LocalDate.of(2021,9,25);
-    LocalDate nextFull, nextNew;
-
-    /*Get time between today and historic full moon & calculate cycles */
-    double elapsed = Duration.between(historic_full_moon.atStartOfDay(), today.atStartOfDay()).toDays();
-    double elapsed_cycles = elapsed/synodic_month;
-    double elapsed_nachkomma = (elapsed_cycles-((int) elapsed_cycles));
-
-    /*multiply percentage remainder times synodic month to get remaining days to the next full moon*/
-    double until_next_full = (1 - elapsed_nachkomma)*synodic_month;
-    nextFull = today.plusDays((long)until_next_full);
-
-    System.out.println("Next full moon is on "+nextFull);
-
-    /*multiply percentage remainder times synodic month to get remaining days to the next full moon*/
-    double until_next_new = (0.5 - elapsed_nachkomma)*synodic_month +1;
-    nextNew = today.plusDays((long)until_next_new);
-
-    System.out.println("Next new moon is on "+nextNew);
+    calculate_moon_dates();
 
     }
 }
