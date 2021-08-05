24f4712ab2f880c703becd414fea2d7707a28797
mjakobs2 <markus.jakobs@smail.th-koeln.de>
2021-08-05 23:31:40 +0100

    implemented new moon calculation; comments / javadoc;

--- src/habedischnak/Main.java
+++ src/habedischnak/Main.java
@@ -3,6 +3,12 @@ package habedischnak;
 import java.time.Duration;
 import java.time.LocalDate;
 
+/**
+ * @author MJ
+ * Progamm zur Berechnung des jeweils kommenden Voll- / Neumondes
+ */
+
+
 public class Main {
     public static void main(String[] args) {
 	// write your code here
@@ -10,7 +16,8 @@ public class Main {
 
 	LocalDate historic_full_moon = LocalDate.of(2000,1,21);
     LocalDate today = LocalDate.now();
-    LocalDate nextFull;
+    //LocalDate today = LocalDate.of(2021,9,25);
+    LocalDate nextFull, nextNew;
 
     /*Get time between today and historic full moon & calculate cycles */
     double elapsed = Duration.between(historic_full_moon.atStartOfDay(), today.atStartOfDay()).toDays();
@@ -21,13 +28,13 @@ public class Main {
     double until_next_full = (1 - elapsed_nachkomma)*synodic_month;
     nextFull = today.plusDays((long)until_next_full);
 
-    System.out.println(today+" "+nextFull);
+    System.out.println("Next full moon is on "+nextFull);
 
+    /*multiply percentage remainder times synodic month to get remaining days to the next full moon*/
+    double until_next_new = (0.5 - elapsed_nachkomma)*synodic_month +1;
+    nextNew = today.plusDays((long)until_next_new);
 
-
-
-
-
+    System.out.println("Next new moon is on "+nextNew);
 
     }
 }
